92626d6957597d6e2164b5d4ee37ce43ef35c994
Soham Bhattacharya <sohamb117@gmail.com>
2020-02-12 20:10:49 +0000

    Good

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,22 +3,23 @@ use rand::Rng;
 use std::cmp::Ordering;
 fn main() {
     println!("Guess the number");
-
-    //let mut topBound = String::new();
-    //io::stdin().read_line(&mut topBound)
-    //    .expect("Failed to read line");
-    //let topBound: u32 = topBound.trim().parse()
-    //    .expect("Please type a number!");
-    //let mut bottomBound = String::new();
-    //io:: stdin().read_line(&mut bottomBound)
-    //    .expect("Failed to read line");
-    //let bottomBound: u32 = bottomBound.trim().parse()
-    //    .expect("Please type a number!");
+	println!("Bottom bound?");
+    let mut bottomBound = String::new();
+    io:: stdin().read_line(&mut bottomBound)
+        .expect("Failed to read line");
+    let bottomBound: u32 = bottomBound.trim().parse()
+        .expect("Please type a number!");
+	println!("Top bound?");
+    let mut topBound = String::new();
+    io::stdin().read_line(&mut topBound)
+        .expect("Failed to read line");
+    let topBound: u32 = topBound.trim().parse()
+        .expect("Please type a number!");
     
-	let number = rand::thread_rng().gen_range(1,101); //same as guess, except not mute and using rand instead of string
+	let number = rand::thread_rng().gen_range(bottomBound,(topBound+1)); //same as guess, except not mute and using rand instead of string
     
     loop {
-        
+        println!("Guess?");
 	    let mut guess = String::new(); //let to create variable, mut to make mutable. Stringg::new makes the variable a string
 	    io::stdin().read_line(&mut guess) //read_line reads the user's input and sets mut guess to the input, & is reference
 		    .expect("Failed to read line"); //response in case of fail
