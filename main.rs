1529df4a1e995109262cb9b0820ef6f116e2ac93
Soham Bhattacharya <sohamb117@gmail.com>
2020-02-13 20:45:35 +0000

    Learned if statements and variables

--- main.rs
+++ main.rs
@@ -1,4 +1,18 @@
+use std::io;
 fn main() {
   println!("Hello World!");
-  let int mut a = 1;
+  let mut a: i8 = 16;
+  println!("{}",a);
+  let mut b = String::new();
+  io::stdin().read_line(&mut b) .expect("Failed to read line");
+  let mut b: i8 = b.trim().parse().expect("Please input a number");
+  if a > b{
+	  println!("A is greater");
+  }
+  else if b > a{
+	  println!("B is greater");
+  }
+  else{
+	  println!("They are the same");
+  }
 }
